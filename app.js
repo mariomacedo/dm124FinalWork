8cad1fc44403cc999dc187a5f3dee821b9a520c0
Mario Macedo <mguilhermemacedo@gmail.com>
2019-12-10 07:29:13 -0300

    Connecting do MLAB

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
 // Routes
 app.get('/', (req, res) =>{
-    res.send('We are on home')
+    res.send('We are on home');
 });
 
+app.get('/posts', (req, res) =>{
+    res.send('We are on posts');
+});
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    { useNewUrlParser: true, useUnifiedTopology: true }, 
+    () => { console.log('Connected to MLab')
+});
+
+// Server
 app.listen(3000);
