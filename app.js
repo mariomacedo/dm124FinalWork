055f451061e1cff5ecba33fed03c458560e1f24a
Mario Macedo <mguilhermemacedo@gmail.com>
2019-12-10 19:44:34 -0300

    Base api

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
-const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Import Routes
+const postsRoute = require('./routes/post');
+app.use('/posts', postsRoute);
 
 // Routes
 app.get('/', (req, res) =>{
     res.send('We are on home');
-});
-
-app.get('/posts', (req, res) =>{
-    res.send('We are on posts');
 });
 
 // Connect to DB
