fad41206deffce3e74d4a5e79ca4a6f2451b1da7
Mario Macedo <mguilhermemacedo@gmail.com>
2019-12-10 22:07:00 -0300

    Shipment model

---
+++ modules/Shipment.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const shipmentSchema = new mongoose.Schema({
+    orderId : {
+        type: String,
+        required: true,
+        min: 6, 
+        max: 255
+    },
+    clientId : {
+        type: String,
+        required: true,
+        min: 6, 
+        max: 255
+    },
+    receiverName: {
+        type: String,
+        required: true,
+        min: 6, 
+        max: 255
+    },
+    receiverCpf: {
+        type: Number,
+        require: true,
+        min: 11,
+        max: 11
+    },
+    isReceiverTheBuyer: {
+        type: Boolean,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    coordinates: {
+        type: [Number],
+        required: true
+    }
+    
+
+});
+
+module.exports = mongoose.model('Shipment', shipmentSchema);
